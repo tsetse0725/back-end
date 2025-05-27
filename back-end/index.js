01617a3dae73bcf83c351633afc5d235e63da500
tsetse0725 <tse9406@gmail.com>
2025-05-27 12:29:37 +0800

    updated

--- back-end/index.js
+++ back-end/index.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const fs = require("fs");
 
-const array = [1, 2, 3, 4];
+app.use(express.json());
 
-app.get("/array", (req, res) => {
-  res.status(200).send(array);
+app.get("/users", (req, res) => {
+  res.status(200).send(users);
 });
 
-app.post("/test", (req, res) => {
-  array.push(5);
+app.post("/user", async (req, res) => {
+  await fs.writeFileSync("./users.json", JSON.stringify(req.body), "utf-8");
   res.status(200).send("successfully");
 });
 
-app.delete("/array", (req, res) => {
-  array.pop();
-  res.status(200).send("deleted");
+app.put("/users", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  dataJSON = { ...result, userName: "Bataa3456" };
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("userName updated");
+});
+
+app.put("/use", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  dataJSON = { ...result, phone: "88776655" };
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("phone updated");
+});
+
+app.post("/us", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  dataJSON = { ...result, job: "softwareEngineer" };
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("job added");
+});
+
+app.delete("/used", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  const { lastName, ...rest } = result;
+  const dataJSON = rest;
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.status(200).send("deleted lastName");
 });
 
 app.listen(port, () => {
